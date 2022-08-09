0ff2ee096837ace3bbe82dfd2b72951eb5b23b25
ryan <ryanjgecelter@gmail.com>
2022-08-09 08:52:27 +0300

    add completed NewTodoForm component. TodoList passes addTodo state update function down to NewTodoForm - passes it back up from onSubmit handler

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-const NewTodoForm = () => {
+const NewTodoForm = ({add}) => {
+    const INITIAL_STATE = {todo:''}
+    const [formData, setFormData] = useState(INITIAL_STATE)
+    const handleChange = (evt) => {
+        evt.preventDefault()
+        const {name, value} = evt.target;
+        setFormData({...formData, [name]: value})
+    }
+    const handleSubmit = evt => {
+        evt.preventDefault()
+        const {todo} = formData
+        add({...formData});
+        setFormData('')
+    }
     return (
         <div>
             <h2>New Todo</h2>
-            <form>
-                    <label>Name</label>
-                    <input/>
+            <form onSubmit={handleSubmit}>
+                    <label htmlFor="todo">Name</label>
+                    <input 
+                        type="text"
+                        name="todo"
+                        value = {formData.todo}
+                        placeholder="Enter todo here"
+                        onChange={handleChange}
+                    />
                     <button>Add Todo</button>
             </form>
         </div>
